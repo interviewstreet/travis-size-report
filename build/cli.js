ff7751f3f63b6e37876dace0ad4e837690c2e807
Aditya Agarwal <adityaa803@gmail.com>
2020-11-03 21:05:39 +0530

    build run

--- build/cli.js
+++ build/cli.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getConfig = void 0;
 const path_1 = __importDefault(require("path"));
 const minimist_1 = __importDefault(require("minimist"));
 const argv = minimist_1.default(process.argv.slice(2), {
