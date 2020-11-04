6ae63b0fb4f9a66f2abd341c890418925a9d4e9c
Aditya Agarwal <adityaa803@gmail.com>
2020-11-04 12:26:12 +0530

    travis deploy setup (#12)

--- build/find-renamed.js
+++ build/find-renamed.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildFindRenamedFunc = void 0;
 const escape_string_regexp_1 = __importDefault(require("escape-string-regexp"));
 const PLACEHOLDER_REGEX = /\\\[(\w+)\\\]/g;
 const REPLACEMENTS = {
--- build/utils.js
+++ build/utils.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getBuildInfo = void 0;
 const util_1 = require("util");
 const fs_1 = require("fs");
 const gzip_size_1 = __importDefault(require("gzip-size"));
